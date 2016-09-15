b78c910533eb43a641690ad173854f349c1ac55a
chrisnatali <chris.natali@gmail.com>
2016-09-15 17:50:07 -0400

    working generator and tester

--- stress.py
+++ stress.py
@@ -2,6 +2,8 @@ import asyncio
 import requests
 import functools
 import copy
+import argparse
+import json
 
 class HttpStress():
     
@@ -11,38 +13,53 @@ class HttpStress():
     
     def async_tests(self):
         """
-        returns a list of futures to be run async by event loop
+        returns a list of coroutines to be run async by event loop
         """
-        futures = []
+        coroutines = []
         for timestep, url, method, data in self.http_tests:
             coroutine = self.make_request(timestep, url, method, data)
-            futures.append(coroutine)
+            coroutines.append(coroutine)
 
-        return futures
+        return coroutines
 
     async def make_request(self, timestep, url, method, data):
-        print("timestep {} method {} url {}".format(timestep, method, url))
         await asyncio.sleep(timestep)
         arg_dict = copy.deepcopy(data)
         # read data from any files 
         if 'files' in arg_dict:
-            file_dict = {attr_name: open(file_name, 'rb').read() for 
-                         attr_name, file_name in arg_dict['files'].items()}
+            file_dict = dict()
+            for attr_name, file_name in arg_dict['files'].items():
+                with open(file_name, 'rb') as fh:
+                    file_dict[attr_name] = (file_name, fh.read())
+
             arg_dict['files'] = file_dict
         partial = functools.partial(requests.request, method, url, **arg_dict)
         response = await self.event_loop.run_in_executor(None, partial)
-        print(timestep, url, method, response)
+        print(','.join(timestep, url, method, response.text))
+
+def convert_record(tup):
+    try:
+        result = (int(tup[0]), str(tup[1]), str(tup[2]), json.loads(tup[3]))
+    except Exception as e:
+        raise Exception("Bad test tuple: {}, error {}".format(tup, e))
+    return result
        
 if __name__ == '__main__':
 
-    loop = asyncio.get_event_loop()
+    # read tests from file 
+    http_tests = []
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t",
+        "--tests_file",
+        help="csv file of timestep,url,method,config_json for each request")
+     
+    args = parser.parse_args()
+    for line in open(args.tests_file, 'r'):
+        raw_record = tuple([field.strip() for field in line.split(',', 3)])
+        test_record = convert_record(raw_record)
+        http_tests.append(test_record)
 
-    stress = HttpStress(
-        http_tests=[
-            (3, 'http://localhost:9000/jobs', 'GET', {'headers': {'Accept': 'application/json'}}),
-            (0, 'http://localhost:9000/status', 'GET', {'headers': {'Accept': 'application/json'}}),
-            (1, 'http://localhost:9000/jobs', 'POST', {'files': {'zip_file': 'sleep_count_8.zip'}, 'data': {'model': 'test', 'job_name': 'sleep_8_1'}, 'headers': {'Accept': 'application/json'}}),
-            ],
-        event_loop=loop)
-    
+    loop = asyncio.get_event_loop()
+    stress = HttpStress(http_tests=http_tests, event_loop=loop)
     asyncio.get_event_loop().run_until_complete(asyncio.wait(stress.async_tests()))
